67365d32059066016a7b820511ffc26407a9ded6
Renaldinho <rena0968@easv365.dk>
2024-03-03 14:20:47 +0100

    add create initial category table and relationship
    added sql script as a new step for creating the category table, and configuring the product table to create the relationship.

---
+++ manual-migrations/RollbackScripts/002_RollbackAddCategoriesTable.sql
@@ -0,0 +1,11 @@
+﻿-- Rollback script to remove the CategoryId column and constraint from the Products table and finally removing categories table 
+-- First, drop the foreign key constraint
+ALTER TABLE Products
+DROP CONSTRAINT IF EXISTS FK_Products_Categories;
+
+-- drop the CategoryId column
+ALTER TABLE Products
+DROP COLUMN IF EXISTS CategoryId;
+     
+--  drop the Categories table
+DROP TABLE IF EXISTS Categories;
