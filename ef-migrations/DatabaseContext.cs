96d8465c7a80e943262cfa5494a0415bcd9daff3
Renaldinho <rena0968@easv365.dk>
2024-03-03 14:41:01 +0100

    add initial create for products table
    added configuration and necessary packages for entity framework, created the initial data model for products entity

---
+++ ef-migrations/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using ef_migrations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ef_migrations;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("DataSource=db.db");
+}
